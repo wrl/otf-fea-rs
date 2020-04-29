c9fc2c23383ebb65d0a6da081faae67c57eae1ff
William Light <git@wrl.lhiaudio.com>
2020-04-30 01:00:21 +0200

    comp_test: add GDEF header structs

--- src/bin/comp_test.rs
+++ src/bin/comp_test.rs
@@ -41,5 +41,38 @@ mod GPOS {
     }
 }
 
+#[allow(non_snake_case, non_camel_case_types)]
+mod GDEF {
+    struct Header_1_0 {
+        major: u16,
+        minor: u16,
+        glyph_class_def_offset: u16,
+        attach_list_offset: u16,
+        lig_caret_list_offset: u16,
+        mark_attach_class_def_offset: u16
+    }
+
+    struct Header_1_2 {
+        major: u16,
+        minor: u16,
+        glyph_class_def_offset: u16,
+        attach_list_offset: u16,
+        lig_caret_list_offset: u16,
+        mark_attach_class_def_offset: u16,
+        mark_glyph_sets_def_offset: u16
+    }
+
+    struct Header_1_3 {
+        major: u16,
+        minor: u16,
+        glyph_class_def_offset: u16,
+        attach_list_offset: u16,
+        lig_caret_list_offset: u16,
+        mark_attach_class_def_offset: u16,
+        mark_glyph_sets_def_offset: u16,
+        item_var_store_offset: u16
+    }
+}
+
 fn main() {
 }
