25f7f3a4f2b8c442d0d7fac1f0212d514e031fa4
William Light <git@wrl.lhiaudio.com>
2020-05-14 20:22:40 +0200

    parse <NULL> value records
    fixes bug1459.fea
    
    passing: 87
    failing: 10

--- src/parse_model/value_record.rs
+++ src/parse_model/value_record.rs
@@ -32,7 +32,60 @@ pub enum ValueRecord {
         y_placement: DeviceAdjustedMetric,
         x_advance: DeviceAdjustedMetric,
         y_advance: DeviceAdjustedMetric
-    }
+    },
+
+    // format D
+    // spec says unimplemented, but feaLib implements it
+    Null
+}
+
+#[inline]
+fn placement_advance<Input>() -> impl Parser<FeaRsStream<Input>, Output = ValueRecord>
+    where Input: Stream<Token = u8>,
+          Input::Error: ParseError<Input::Token, Input::Range, Input::Position>
+{
+    metric()
+        .skip(required_whitespace())
+        .and(metric())
+        .skip(required_whitespace())
+        .and(metric())
+        .skip(required_whitespace())
+        .and(metric())
+        .and(choice((
+            required_whitespace()
+                .with(device())
+                .skip(required_whitespace())
+                .and(device())
+                .skip(required_whitespace())
+                .and(device())
+                .skip(required_whitespace())
+                .and(device())
+                .skip(optional_whitespace())
+                .skip(token(b'>'))
+                .map(|(((d1, d2), d3), d4)| Some((d1, d2, d3, d4))),
+
+            optional_whitespace()
+                .skip(token(b'>'))
+                .map(|_| None)
+        )))
+
+        .map(|((((x_placement, y_placement), x_advance), y_advance), devices)| {
+            if let Some(devices) = devices {
+                ValueRecord::DeviceAdjusted {
+                    x_placement: DeviceAdjustedMetric::new(x_placement, devices.0),
+                    y_placement: DeviceAdjustedMetric::new(y_placement, devices.1),
+                    x_advance: DeviceAdjustedMetric::new(x_advance, devices.2),
+                    y_advance: DeviceAdjustedMetric::new(y_advance, devices.3)
+                }
+            } else {
+                ValueRecord::PlacementAdvance {
+                    x_placement,
+                    y_placement,
+                    x_advance,
+                    y_advance,
+                }
+            }
+        })
 }
 
 pub(crate) fn value_record<Input>() -> impl Parser<FeaRsStream<Input>, Output = ValueRecord>
@@ -40,46 +93,15 @@ pub(crate) fn value_record<Input>() -> impl Parser<FeaRsStream<Input>, Output = 
           Input::Error: ParseError<Input::Token, Input::Range, Input::Position>
 {
     choice((
-        token(b'<').skip(optional_whitespace())
-            .with(metric()).skip(required_whitespace())
-            .and(metric()).skip(required_whitespace())
-            .and(metric()).skip(required_whitespace())
-            .and(metric())
-            .and(choice!(
-                    required_whitespace()
-                        .with(device())
-                        .skip(required_whitespace())
-                        .and(device())
-                        .skip(required_whitespace())
-                        .and(device())
-                        .skip(required_whitespace())
-                        .and(device())
-                        .skip(optional_whitespace())
-                        .skip(token(b'>'))
-                        .map(|(((d1, d2), d3), d4)| Some((d1, d2, d3, d4))),
+        token(b'<')
+            .with(choice((
+                literal("NULL")
+                    .skip(token(b'>'))
+                    .map(|_| ValueRecord::Null),
 
-                    optional_whitespace()
-                        .skip(token(b'>'))
-                        .map(|_| None)
-            ))
-
-            .map(|((((x_placement, y_placement), x_advance), y_advance), devices)| {
-                if let Some(devices) = devices {
-                    ValueRecord::DeviceAdjusted {
-                        x_placement: DeviceAdjustedMetric::new(x_placement, devices.0),
-                        y_placement: DeviceAdjustedMetric::new(y_placement, devices.1),
-                        x_advance: DeviceAdjustedMetric::new(x_advance, devices.2),
-                        y_advance: DeviceAdjustedMetric::new(y_advance, devices.3)
-                    }
-                } else {
-                    ValueRecord::PlacementAdvance {
-                        x_placement,
-                        y_placement,
-                        x_advance,
-                        y_advance,
-                    }
-                }
-            }),
+                optional_whitespace()
+                    .with(placement_advance()),
+            ))),
 
         metric()
             .map(ValueRecord::Advance)
