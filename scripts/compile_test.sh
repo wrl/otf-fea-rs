3454750372424eff70bd22b5fba9c8ba013e77a5
William Light <git@wrl.lhiaudio.com>
2020-06-10 20:16:59 +0200

    add scripts/compile_test.sh

---
+++ scripts/compile_test.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+RUNNER=./target/debug/compile
+TESTS_DIR=../fonttools/Tests/feaLib/data
+OUT_DIR=./compiler_test_out
+
+one_test() {
+	p="$TESTS_DIR/$*"
+	fea="$p.fea"
+	ttf="$p.ttf"
+	out="./$*.fea-rs.ttf"
+
+	echo "$fea"
+	echo ''
+	echo '------------------------'
+	cat "$fea"
+	echo '------------------------'
+	echo ''
+
+	if $RUNNER "$fea" "$out"; then
+		if diff "$out" "$ttf"; then
+			echo 'pass!';
+			return 0
+		fi
+	fi
+	return 1
+}
+
+case "$1" in
+	"")
+		echo 'need a test file'
+		exit 1
+		;;
+
+	*)
+		[ "$2" == "-f" ] && exec nvim "$TESTS_DIR/$1.fea"
+		[ "$2" == "-x" ] && exec nvim "$TESTS_DIR/$1.ttx"
+		one_test "$1"
+		exit $?
+		;;
+esac
