74ae05f3e128cd86876264b21dcea36eb936d31e
William Light <git@wrl.lhiaudio.com>
2020-06-21 06:02:08 +0200

    compile_model/value_record: simpler decode_from_be_bytes()

--- src/compile_model/value_record.rs
+++ src/compile_model/value_record.rs
@@ -44,33 +44,42 @@ impl ValueRecord {
     // of their fields - down to and including 0 fields in some cases. the presence of each field
     // in the encoded representation is indicated by a set bit flag in the `format` variable.
 
-    pub fn decode_from_be_bytes(bytes: &[u8], mut format: u16) -> Self {
-        let mut buf = [0u16; 8];
-        let mut buf_idx = 0;
+    #[allow(unused_assignments)]
+    pub fn decode_from_be_bytes(bytes: &[u8], format: u16) -> Self {
+        let mut ret = Self {
+            x_placement: 0,
+            y_placement: 0,
+            x_advance: 0,
+            y_advance: 0,
+
+            x_placement_device_offset: 0,
+            y_placement_device_offset: 0,
+            x_advance_device_offset: 0,
+            y_advance_device_offset: 0
+        };
+
         let mut bytes_idx = 0;
 
-        format &= 0x00FF;
-
-        while format > 0 {
-            let first_set = format.trailing_zeros() + 1;
-            buf_idx += first_set as usize;
-            format >>= first_set;
-
-            buf[buf_idx - 1] = decode_u16_be(bytes, bytes_idx);
-            bytes_idx += 2;
+        macro_rules! read_if_in_format {
+            ($shift:expr, $var:ident, $t:ty) => {
+                if (format & (1u16 << $shift)) != 0 {
+                    ret.$var = decode_u16_be(bytes, bytes_idx) as $t;
+                    bytes_idx += 2;
+                }
+            }
         }
 
-        Self {
-            x_placement: buf[0] as i16,
-            y_placement: buf[1] as i16,
-            x_advance: buf[2] as i16,
-            y_advance: buf[3] as i16,
+        read_if_in_format!(0, x_placement, i16);
+        read_if_in_format!(1, y_placement, i16);
+        read_if_in_format!(2, x_advance, i16);
+        read_if_in_format!(3, y_advance, i16);
 
-            x_placement_device_offset: buf[4],
-            y_placement_device_offset: buf[5],
-            x_advance_device_offset: buf[6],
-            y_advance_device_offset: buf[7]
-        }
+        read_if_in_format!(4, x_placement_device_offset, u16);
+        read_if_in_format!(5, y_placement_device_offset, u16);
+        read_if_in_format!(6, x_advance_device_offset, u16);
+        read_if_in_format!(7, y_advance_device_offset, u16);
+
+        ret
     }
 
     #[allow(unused_assignments)]
@@ -79,7 +88,7 @@ impl ValueRecord {
 
         macro_rules! write_if_in_format {
             ($shift:expr, $var:ident) => {
-                if (format & (1u16 << $shift)) > 0 {
+                if (format & (1u16 << $shift)) != 0 {
                     &bytes[bytes_idx..bytes_idx + 2]
                         .copy_from_slice(&self.$var.to_be_bytes());
                     bytes_idx += 2;
