3e7da8f6cea3bf9d2c31bc4c4cbd1aa3a496599f
William Light <git@wrl.lhiaudio.com>
2020-09-14 19:18:35 +0200

    compile/anchor: codec implementation

--- src/compile_model/tables/gpos/anchor.rs
+++ src/compile_model/tables/gpos/anchor.rs
@@ -1,5 +1,8 @@
-use crate::parse_model as pm;
+use endian_codec::{EncodeBE, DecodeBE, PackedSize};
 
+use crate::compile_model::util::encode::*;
+use crate::compile_model::util::decode::*;
+use crate::parse_model as pm;
 
 #[derive(Debug, Clone)]
 pub enum Anchor {
@@ -44,5 +47,72 @@ impl From<&pm::Anchor> for Anchor {
 
             a => panic!("anchor try_from unimplemented for {:?}", a)
         }
+    }
+}
+
+#[derive(EncodeBE, DecodeBE, PackedSize)]
+struct AnchorFormat1 {
+    format: u16,
+    x: i16,
+    y: i16
+}
+
+impl From<AnchorFormat1> for Anchor {
+    fn from(encoded: AnchorFormat1) -> Self {
+        Self::Coord {
+            x: encoded.x,
+            y: encoded.y
+        }
+    }
+}
+
+#[derive(EncodeBE, DecodeBE, PackedSize)]
+struct AnchorFormat2 {
+    format: u16,
+    x: i16,
+    y: i16,
+    contour_point: u16
+}
+
+impl From<AnchorFormat2> for Anchor {
+    fn from(encoded: AnchorFormat2) -> Self {
+        Self::ContourCoord {
+            x: encoded.x,
+            y: encoded.y,
+            contour_point: encoded.contour_point
+        }
+    }
+}
+
+impl TTFEncode for Anchor {
+    fn ttf_encode(&self, buf: &mut EncodeBuf) -> EncodeResult<usize> {
+        match self {
+            &Self::Coord { x, y } =>
+                buf.append(&AnchorFormat1 {
+                    format: 1,
+                    x,
+                    y
+                }),
+
+            &Self::ContourCoord { x, y, contour_point } =>
+                buf.append(&AnchorFormat2 {
+                    format: 1,
+                    x,
+                    y,
+                    contour_point
+                }),
+        }
+    }
+}
+
+impl TTFDecode for Anchor {
+    fn ttf_decode(bytes: &[u8]) -> DecodeResult<Self> {
+        let format = decode_u16_be(bytes, 0);
+
+        Ok(match format {
+            1 => decode_from_slice::<AnchorFormat1>(bytes).into(),
+            2 => decode_from_slice::<AnchorFormat2>(bytes).into(),
+            _ => return Err(DecodeError::InvalidValue("format", "Anchor".into()))
+        })
     }
 }
