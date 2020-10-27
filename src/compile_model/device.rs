fd94704378f659a5a114c2b1d593ee5f03897720
William Light <git@wrl.lhiaudio.com>
2020-10-27 20:32:43 +0100

    compiler/device: TTFEncode impl

--- src/compile_model/device.rs
+++ src/compile_model/device.rs
@@ -1,6 +1,8 @@
 use std::convert::TryFrom;
 use std::collections::BTreeMap;
 
+use endian_codec::{PackedSize, EncodeBE, DecodeBE};
+
 use crate::compile_model::util::encode::*;
 use crate::compile_model::error::*;
 
@@ -10,7 +12,7 @@ use crate::parse_model as pm;
 #[derive(Debug, PartialEq, Eq, Clone)]
 pub struct Device {
     // ppem_size => pixel_adjustment
-    pub adjustments: BTreeMap<i8, i8>
+    pub adjustments: BTreeMap<u16, i8>
 }
 
 impl TryFrom<&pm::Device> for Device {
@@ -21,9 +23,9 @@ impl TryFrom<&pm::Device> for Device {
 
         if let pm::Device::Adjustments(ref parsed_adj) = parsed {
             for adj in parsed_adj {
-                let ppem_size = i8::try_from(adj.ppem_size)
+                let ppem_size = u16::try_from(adj.ppem_size)
                     .map_err(|_| CompileError::Overflow {
-                        ty: "i8",
+                        ty: "u16",
                         scope: "Device".into(),
                         item: "ppem_size",
                         value: adj.ppem_size as usize
@@ -47,9 +49,157 @@ impl TryFrom<&pm::Device> for Device {
     }
 }
 
+#[repr(u16)]
+#[derive(Debug, Clone, Copy)]
+enum DeltaFormat {
+    TwoBit   = 0x0001,
+    FourBit  = 0x0002,
+    EightBit = 0x0003,
+}
+
+#[derive(Debug, PackedSize, EncodeBE, DecodeBE)]
+struct DeviceHeader {
+    pub start_ppem: u16,
+    pub end_ppem: u16,
+    pub delta_format: u16
+}
+
+macro_rules! pack_ppem_base {
+    ($self:ident, $range:ident, $done:ident, $bits:expr) => {
+        match $range.next() {
+            Some(ppem) => match $self.adjustments.get(&ppem) {
+                Some(adj) => {
+                    let adj = *adj as u16;
+                    let sign = ((adj & (1 << 7)) != 0) as u16;
+                    let mask = (1 << ($bits - 2)) - 1;
+
+                    (adj & mask) | (sign << ($bits - 1))
+                },
+                None => 0u16
+            },
+            None => {
+                $done = true;
+                0u16
+            }
+        }
+    }
+}
+
+impl Device {
+    fn pack_eights(&self, start_ppem: u16, end_ppem: u16, buf: &mut EncodeBuf) -> EncodeResult<()> {
+        let mut range = start_ppem ..= end_ppem;
+        let mut done = false;
+
+        macro_rules! pack_ppem {
+            () => {
+                pack_ppem_base!(self, range, done, 8)
+            }
+        }
+
+        while !done {
+            let packed: u16 =
+                 (pack_ppem!() << 8)
+                | pack_ppem!();
+
+            buf.append(&packed)?;
+        }
+
+        Ok(())
+    }
+
+    fn pack_fours(&self, start_ppem: u16, end_ppem: u16, buf: &mut EncodeBuf) -> EncodeResult<()> {
+        let mut range = start_ppem ..= end_ppem;
+        let mut done = false;
+
+        macro_rules! pack_ppem {
+            () => {
+                pack_ppem_base!(self, range, done, 4)
+            }
+        }
+
+        while !done {
+            let packed: u16 =
+                  (pack_ppem!() << 12)
+                | (pack_ppem!() << 8)
+                | (pack_ppem!() << 4)
+                |  pack_ppem!();
+
+            buf.append(&packed)?;
+        }
+
+        Ok(())
+    }
+
+    fn pack_twos(&self, start_ppem: u16, end_ppem: u16, buf: &mut EncodeBuf) -> EncodeResult<()> {
+        let mut range = start_ppem ..= end_ppem;
+        let mut done = false;
+
+        macro_rules! pack_ppem {
+            () => {
+                pack_ppem_base!(self, range, done, 2)
+            }
+        }
+
+        while !done {
+            let packed: u16 =
+                  (pack_ppem!() << 14)
+                | (pack_ppem!() << 12)
+                | (pack_ppem!() << 10)
+                | (pack_ppem!() << 8)
+                | (pack_ppem!() << 6)
+                | (pack_ppem!() << 4)
+                | (pack_ppem!() << 2)
+                |  pack_ppem!();
+
+            buf.append(&packed)?;
+        }
+
+        Ok(())
+    }
+}
+
 impl TTFEncode for Device {
     fn ttf_encode(&self, buf: &mut EncodeBuf) -> EncodeResult<usize> {
         let start = buf.bytes.len();
+        let mut format = DeltaFormat::TwoBit;
+
+        if self.adjustments.len() == 0 {
+            // FIXME: what do we do here? encode an empty device table, or pass some information up
+            // to the caller?
+
+            buf.append(&DeviceHeader {
+                start_ppem: 0,
+                end_ppem: 0,
+                delta_format: DeltaFormat::EightBit as u16
+            })?;
+            buf.append(&0u16)?;
+        }
+
+        let start_ppem = *self.adjustments.keys().next().unwrap();
+        let end_ppem = *self.adjustments.keys().last().unwrap();
+
+        for v in self.adjustments.values() {
+            let v = *v;
+
+            if v > 7 || v < -8 {
+                format = DeltaFormat::EightBit;
+            } else if v > 1 || v < -2 {
+                format = DeltaFormat::FourBit;
+            }
+        }
+
+        buf.append(&DeviceHeader {
+            start_ppem,
+            end_ppem,
+            delta_format: format as u16
+        })?;
+
+        match format {
+            DeltaFormat::TwoBit => self.pack_twos(start_ppem, end_ppem, buf)?,
+            DeltaFormat::FourBit => self.pack_fours(start_ppem, end_ppem, buf)?,
+            DeltaFormat::EightBit => self.pack_eights(start_ppem, end_ppem, buf)?,
+        };
+
         Ok(start)
    }
 }
